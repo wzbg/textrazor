988534f9c3c530adefbd669a62f3395eebee90da
zyc <z35305515@gmail.com>
2016-03-14 19:43:04 +0800

    long content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: zyc
 * @Date:   2016-02-18 14:06:33
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-03-14 17:57:32
+* @Last Modified time: 2016-03-14 19:36:29
 */
 'use strict'
 
@@ -18,11 +18,44 @@ module.exports = class {
     if (!headers['Content-Type']) headers['Content-Type'] = 'text/html'
     this.apiKey = headers['x-textrazor-key']
   }
+
   exec(text, options) {
     if (typeof text === 'object') options = text
     options = options || {}
     options.extractors = options.extractors || 'entities'
-    if (!options.text) options.text = text
+    if (options.text) text = options.text
+    const maxLength = options.maxLength || 100000
+    let tmpText = ''
+    const texts = []
+    for (let content of text.split(' ')) {
+      tmpText += content + ' '
+      if (tmpText.length > maxLength) {
+        texts.push(tmpText)
+        tmpText = ''
+      }
+    }
+    if (tmpText) texts.push(tmpText)
+    return new Promise((resolve, reject) => {
+      Promise.all(texts.map(text => {
+        options.text = text
+        return this.textrazor(options)
+      })).then(results => {
+        let json
+        for (let data of results) {
+          if (json) {
+            for (let entity of data.response.entities) {
+              json.response.entities.push(entity)
+            }
+          } else {
+            json = data
+          }
+        }
+        resolve(json)
+      }).catch(err => reject(err))
+    })
+  }
+
+  textrazor(options) {
     return new Promise((resolve, reject) => {
       request.post(url, { headers: this.headers, form: options }, (err, resp, body) => {
         if (err) return reject(err)
